08752ac220c31bec795d7af42591bea92d5bc273
Deb Banerji <debkbanerji@gmail.com>
2017-03-25 13:51:52 -0400

    Updating Store Name

--- web/mapingo/app/manage-store/manage-store.component.js
+++ web/mapingo/app/manage-store/manage-store.component.js
@@ -4,6 +4,35 @@ angular.module('manageStore').component('manageStore', {
     controller: ['$routeParams', '$route', '$firebaseObject', '$firebaseArray', function manageStoreController($routeParams, $route, $firebaseObject, $firebaseArray) {
         var self = this;
         var user = firebase.auth().currentUser;
-        self.manageStoreRef = firebase.database().ref().child("users").child(user.uid).child("manage-store");
+        self.storeRef = firebase.database().ref().child("shops").child(user.uid);
+        console.log(user.uid);
+        self.storeName = "Loading...";
+        self.newStoreName = "";
+
+        self.nameRefObject = $firebaseObject(self.storeRef.child("name"));
+        self.nameRefObject.$loaded(
+            function(data) {
+                val = data.$value;
+                var defaultStoreName = user.displayName + "\'s store";
+                self.storeName = defaultStoreName;
+                if (val == null || val == "") {
+                    // self.storeName = defaultStoreName;
+                    self.storeRef.child("name").set(defaultStoreName);
+                    $route.reload()
+                } else {
+                    self.storeName = val;
+                }
+            },
+            function(error) {
+                console.error("Error:", error);
+            }
+        );
+
+        self.updateStoreName = function () {
+            if (null != self.newStoreName && self.newStoreName != "") {
+                self.storeRef.child("name").set(self.newStoreName);
+                $route.reload();
+            }
+        }
     }]
 });
